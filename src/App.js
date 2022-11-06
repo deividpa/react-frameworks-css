2106066104149e725809e2c3ee9b70b4c394fd0e
David Pérez <deividpa022@gmail.com>
2022-11-06 15:23:14 -0500

    Creación de una MaterialUI Card a través de librería de MaterialUI instalada por medio de NPM. Y se agrega pack de iconos y fuente Robocop de Material

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 // Librerías locales
 
 import Bootstrap from "./Components/Bootstrap";
+import MaterialUI from "./Components/MaterialCard";
 
 function App() {
   return (
     <div className="App">
       <h1>Framework CSS con React</h1>
       <Bootstrap />
+      <hr />
+      <MaterialUI />
     </div>
   );
 }
--- src/Components/MaterialUI.js
+++ src/Components/MaterialUI.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MediaCard from "./MaterialCard";
 
 export default function MaterialUI() {
   return (
     <>
       <h3>Estilizando con librería Material-UI</h3>
+      <MediaCard />
     </>
   );
 }
